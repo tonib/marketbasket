c72d94f5d12343d2137e155ca7d1f5cc5def169e
Toni Bennasar <toni.bennasar@gmail.com>
2020-09-04 20:12:52 +0200

    Moved dataset functions to DataSet class

--- dataset.py
+++ dataset.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+from settings import Settings
+from labels import Labels
 
 class DataSet:
 
@@ -6,4 +9,75 @@ class DataSet:
 
     # Train dataset file path
     EVAL_DATASET_FILE = 'data/dataset_eval.tfrecord'
+
+    @staticmethod
+    def setup_feature_keys(items_labels: Labels, customer_labels: Labels):
+        # Feature mappings for training
+        DataSet.keys_to_features = {
+            'input_items_idx': tf.io.RaggedFeature(tf.int64, 'input_items_idx'),
+            'output_item_idx': tf.io.FixedLenFeature([], tf.int64)
+        }
+        if Settings.N_MAX_CUSTOMERS > 0:
+            DataSet.keys_to_features['customer_idx'] = tf.io.FixedLenFeature([], tf.int64)
+
+        DataSet.n_items = len(items_labels.labels)
+        DataSet.n_customers = len(customer_labels.labels)
+
+    @staticmethod
+    @tf.function
+    def raged_lists_batch_to_multihot(ragged_lists_batch, multihot_dim):
+        t = tf.one_hot( ragged_lists_batch , multihot_dim )
+        t = tf.reduce_max( t , axis=1 )
+        return t
+
+    @staticmethod
+    @tf.function
+    def example_parse_function(proto_batch):
+        # Load one example
+        parsed_features = tf.io.parse_example(proto_batch, DataSet.keys_to_features)
+
+        # Map batch of list of item indices to a batch of multi-hot arrays
+        # Ex [ [1, 2] , [2, 3] ] -> [ [ 0 , 1 , 1 , 0 ] , [ 0 , 0 , 1 , 1 ] ]
+        items_input = parsed_features['input_items_idx']
+        items_input = DataSet.raged_lists_batch_to_multihot( items_input , DataSet.n_items )
+
+        if Settings.N_MAX_CUSTOMERS > 0:
+            # Batch of customer indices. Ex: [1, 2]
+            customer = parsed_features['customer_idx']
+            # Batch of one hot encodings. Ex: [ [0 , 1 , 0], [0, 0, 1] ]
+            customer = tf.one_hot(customer, DataSet.n_customers)
+            input = { 'input_items_idx': items_input , 'customer_idx': customer }
+        else:
+            input = items_input
+
+        # Return tuple (net input, expected output)
+        return input, parsed_features['output_item_idx']
+            
+    @staticmethod
+    def n_eval_batches() -> int:
+        # We need the batches number in evaluation dataset, so here is:
+        # (This will be executed in eager mode)
+        train_dataset = tf.data.TFRecordDataset( [ DataSet.EVAL_DATASET_FILE ] )
+        train_dataset = train_dataset.batch( Settings.BATCH_SIZE )
+        for n_eval_batches, _ in enumerate(train_dataset):
+            pass
+        # print(f'Number of elements: {n_eval_batches}')
+        return n_eval_batches
+
+    @staticmethod
+    def load_train_dataset() -> tf.data.Dataset:
+        train_dataset = tf.data.TFRecordDataset( [ DataSet.TRAIN_DATASET_FILE ] )
+        train_dataset = train_dataset.prefetch(10000)
+        train_dataset = train_dataset.shuffle(10000).batch( Settings.BATCH_SIZE )
+        train_dataset = train_dataset.map( DataSet.example_parse_function , num_parallel_calls=8 )
+        return train_dataset
+
+    @staticmethod
+    def load_eval_dataset() -> tf.data.Dataset:
+        eval_dataset = tf.data.TFRecordDataset( [ DataSet.EVAL_DATASET_FILE ] )
+        eval_dataset = eval_dataset.prefetch(10000)
+        eval_dataset = eval_dataset.batch( Settings.BATCH_SIZE )
+        eval_dataset = eval_dataset.map( DataSet.example_parse_function )
+        return eval_dataset
+
     
--- train.py
+++ train.py
@@ -6,76 +6,31 @@ from settings import Settings
 from model import create_model
 from dataset import DataSet
 
+# To test with GPU disabled set environment variable CUDA_VISIBLE_DEVICES=-1
+
 # We need the batches number in evaluation dataset, so here is:
 # (This will be executed in eager mode)
-train_dataset = tf.data.TFRecordDataset( [ DataSet.EVAL_DATASET_FILE ] )
-train_dataset = train_dataset.batch( Settings.BATCH_SIZE )
-for n_eval_batches, _ in enumerate(train_dataset):
-    pass
-# print(f'Number of elements: {n_eval_batches}')
-# exit()
+n_eval_batches = DataSet.n_eval_batches()
 
 # Be sure we use Graph mode (performance)
 disable_eager_execution()
 
 # Load labels
 item_labels = Labels.load(Labels.ITEM_LABELS_FILE)
-N_ITEMS = len( item_labels.labels )
 customer_labels = Labels.load(Labels.CUSTOMER_LABELS_FILE)
-N_CUSTOMERS = len( customer_labels.labels )
 
-# Feature mappings for training
-keys_to_features = {
-    'input_items_idx': tf.io.RaggedFeature(tf.int64, 'input_items_idx'),
-    'output_item_idx': tf.io.FixedLenFeature([], tf.int64)
-}
-if Settings.N_MAX_CUSTOMERS > 0:
-    keys_to_features['customer_idx'] = tf.io.FixedLenFeature([], tf.int64)
-
-
-@tf.function
-def raged_lists_batch_to_multihot(ragged_lists_batch, multihot_dim):
-    t = tf.one_hot( ragged_lists_batch , multihot_dim )
-    t = tf.reduce_max( t , axis=1 )
-    return t
-
-@tf.function
-def example_parse_function(proto_batch):
-    # Load one example
-    parsed_features = tf.io.parse_example(proto_batch, keys_to_features)
-
-    # Map batch of list of item indices to a batch of multi-hot arrays
-    # Ex [ [1, 2] , [2, 3] ] -> [ [ 0 , 1 , 1 , 0 ] , [ 0 , 0 , 1 , 1 ] ]
-    items_input = parsed_features['input_items_idx']
-    items_input = raged_lists_batch_to_multihot( items_input , N_ITEMS )
-
-    if Settings.N_MAX_CUSTOMERS > 0:
-        # Batch of customer indices. Ex: [1, 2]
-        customer = parsed_features['customer_idx']
-        # Batch of one hot encodings. Ex: [ [0 , 1 , 0], [0, 0, 1] ]
-        customer = tf.one_hot(customer, N_CUSTOMERS)
-        input = { 'input_items_idx': items_input , 'customer_idx': customer }
-    else:
-        input = items_input
-
-    # Return tuple (net input, expected output)
-    return input, parsed_features['output_item_idx']
+# Setup data set global variables
+DataSet.setup_feature_keys(item_labels, customer_labels)
 
 # Define train dataset
-train_dataset = tf.data.TFRecordDataset( [ DataSet.TRAIN_DATASET_FILE ] )
-train_dataset = train_dataset.prefetch(10000)
-train_dataset = train_dataset.shuffle(10000).batch( Settings.BATCH_SIZE )
-train_dataset = train_dataset.map( example_parse_function , num_parallel_calls=8 )
+train_dataset = DataSet.load_train_dataset()
 
 # Define evaluation dataset
-eval_dataset = tf.data.TFRecordDataset( [ DataSet.EVAL_DATASET_FILE ] )
-eval_dataset = eval_dataset.prefetch(10000)
-eval_dataset = eval_dataset.batch( Settings.BATCH_SIZE )
-eval_dataset = eval_dataset.map( example_parse_function )
+eval_dataset = DataSet.load_eval_dataset()
 
+# Create model
 model = create_model(item_labels, customer_labels)
 
-# TODO: check loss function...
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
               metrics=['accuracy'])
