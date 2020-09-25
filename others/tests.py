b3e11d4d83d50ba8953987b6b6c815a1acfefbeb
Toni Bennasar <toni.bennasar@gmail.com>
2020-09-26 00:50:30 +0200

    New sequential model version

--- others/tests.py
+++ others/tests.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
 
-t = tf.constant([ "a", "b"])
-t = tf.expand_dims(t, axis=0)
-print(t)
-print( tf.RaggedTensor.from_tensor(t) )
+# batch size = 2, seq size = 3 embeding size = 4
+embedded_items = tf.constant( [ [[1,2,3,4] , [5,6,7,8] , [9,10,11,12]] , [[1,2,3,44] , [5,6,7,88] , [9,10,11,122]] ] )
+# batch size = 2, embeding size = 2
+embedded_customer = tf.constant( [ [30, 31] , [40, 41] ] )
+
+embedded_items_shape = tf.shape(embedded_items)
+batch_size = embedded_items_shape[0]
+sequence_length = embedded_items_shape[1]
+
+embedded_customer = tf.repeat(embedded_customer, sequence_length , axis=0 )
+embedded_customer = tf.reshape( embedded_customer , [ batch_size , sequence_length , -1 ] )
+
+#print( tf.shape(t1) )
+print(embedded_customer)
+result = tf.concat( [embedded_items, embedded_customer] , axis=2)
+print(result)
