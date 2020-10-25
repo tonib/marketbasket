99bf1bfb39cf3c203720140b8286a396407455e5
tonib <tonib@lsisa.com>
2020-10-25 11:17:31 +0100

    Script to train model in Windows

--- buildall.bat
+++
@@ -1,6 +0,0 @@
-REM .\venv\Scripts\activate
-python preprocess.py
-python gen_dataset.py
-python train.py
-python export.py
-python export_serving.py
---
+++ buildall.ps1
@@ -0,0 +1,25 @@
+
+# Activate virtual environment
+.\venv\Scripts\activate
+
+# Stop IIS prediction website (needs to be run as administrator)
+Stop-Website -Name MarketBasketTensorflow
+
+# Generate transactions file
+$CurrentDir = Get-Location
+Set-Location -Path "O:\Programacion\Oscar\Desarrollo\Aplicaciones\Construccion\GXalmacSQL_vX3.Bongrup.1236\DATA002\bin"
+$TargetFile = "$CurrentDir\Data\transactions.txt"
+& ".\aprastenexptrn.exe" $TargetFile
+
+# Go back to python dir.
+Set-Location -Path $CurrentDir
+
+# Generate model from data
+python preprocess.py
+python gen_dataset.py
+python train.py --verbose=2
+python export.py
+python export_serving.py
+
+# Start IIS prediction website (needs to be run as administrator)
+Start-Website -Name MarketBasketTensorflow
